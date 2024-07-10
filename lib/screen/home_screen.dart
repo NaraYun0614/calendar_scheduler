9e89ff9939bf24a4821ede4ada4bdfac24fea3a4
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-07-10 12:42:14 +0900

    Select a date from the calendar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
-    );
-  }
+  State<HomeScreen> createState() => _HomeScreenState();
 }
+
+  class _HomeScreenState extends State<HomeScreen> {
+    DateTime? selectedDay;
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: SafeArea(child: TableCalendar(
+          focusedDay: DateTime(2024, 3, 1),
+          firstDay: DateTime(1800),
+          lastDay: DateTime(3000),
+          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+            setState(() {
+              this.selectedDay = selectedDay;
+            });
+          },
+          selectedDayPredicate: (DateTime date) {
+            if(selectedDay == null) {
+              return false;
+            }
+            return date.isAtSameMomentAs(selectedDay!);
+          },
+        ),
+        ),
+      );
+    }
+  }
