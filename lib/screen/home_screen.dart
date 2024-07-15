1b8b7cbcc16d7409bdbbce59dfc90e8858a684c9
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-07-15 10:07:57 +0900

    Calendar Style

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -13,11 +14,56 @@ class HomeScreen extends StatefulWidget {
 
     @override
     Widget build(BuildContext context) {
+      final defaultBoxDecoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(6.0),
+        border: Border.all(
+          color: Colors.grey[200]!,
+          width: 1.0,
+        ),
+      );
+
+      final defaultTextStyle = TextStyle(
+        color: Colors.grey[600],
+        fontWeight: FontWeight.w700,
+      );
       return Scaffold(
-        body: SafeArea(child: TableCalendar(
-          focusedDay: DateTime(2024, 3, 1),
-          firstDay: DateTime(1800),
-          lastDay: DateTime(3000),
+        body: SafeArea(
+          child: TableCalendar(
+            focusedDay: DateTime(2024, 7, 1),
+            firstDay: DateTime(1800),
+            lastDay: DateTime(3000),
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w700,
+            ),
+          ),
+          calendarStyle: CalendarStyle(
+            isTodayHighlighted: true,
+            defaultDecoration: defaultBoxDecoration,
+            weekendDecoration: defaultBoxDecoration,
+            selectedDecoration: defaultBoxDecoration.copyWith(
+              border: Border.all(
+                color: primaryColor,
+                width: 1.0,
+              ),
+            ),
+            todayDecoration: defaultBoxDecoration.copyWith(
+              color: primaryColor,
+            ),
+            outsideDecoration: defaultBoxDecoration.copyWith(
+              border: Border.all(
+                color: Colors.transparent,
+              )
+            ),
+            defaultTextStyle: defaultTextStyle,
+            weekendTextStyle: defaultTextStyle,
+            selectedTextStyle: defaultTextStyle.copyWith(
+              color: primaryColor,
+            ),
+          ),
           onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
             setState(() {
               this.selectedDay = selectedDay;
