b5fef2ae4de28e60c7bba3a56f14905f7d186f5a
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-07-16 15:47:33 +0900

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -83,24 +83,31 @@ class HomeScreen extends StatefulWidget {
               Expanded(
                   child: Padding(
                     padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-                    child: ListView(
-                      children: schedules.containsKey(selectedDay) ?
-                          schedules[selectedDay]!
-                          .map(
-                              (e) => ScheduleCard(
-                                  startTime: e.startTime,
-                                  endTime: e.endTime,
-                                  content: e.content,
-                                  color: Color(
-                                    int.parse(
-                                      'FF${e.color}',
-                                      radix: 16,
-                                    ),
-                                  ),
+                    child: ListView.separated(
+                      itemCount: schedules.containsKey(selectedDay)
+                        ? schedules[selectedDay]!.length
+                        : 0,
+                      itemBuilder: (BuildContext context, int index) {
+                        /// 선택된 날짜에 해당되는 일정 리스트로 저장
+                        /// List<Schedule>
+                        final selectedSchedules = schedules[selectedDay]!;
+                        final scheduleModel = selectedSchedules[index];
+
+                        return ScheduleCard(
+                            startTime: scheduleModel.startTime,
+                            endTime: scheduleModel.endTime,
+                            content: scheduleModel.content,
+                            color: Color(
+                              int.parse(
+                                'FF${scheduleModel.color}',
+                                radix: 16,
                               ),
-                            )
-                            .toList()
-                        : [],
+                            ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(height: 8.0);
+                      },
                     ),
                   ),
               ),
