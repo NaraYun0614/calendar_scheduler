05613accd54912e4bf19c184a4327427694fb2e1
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-07-16 12:11:22 +0900

    Category Color Rendering

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF0DB2B2);
+
+const categoryColors = {
+  /// red
+  'F44336',
+  /// orange
+  'FF9800',
+  /// yellow
+  'FFEB38',
+  /// green
+  'FCAF50',
+  /// blue
+  '2196F3',
+  /// dark blue
+  '3F5185',
+  /// purple
+  '9C2780',
+};
