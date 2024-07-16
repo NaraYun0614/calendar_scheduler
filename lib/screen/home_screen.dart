81c24f9bad9e309d742c6a580a36fa6b52bb37d9
NaraYun0614 <nara.yun@mail.mcgill.ca>
2024-07-16 11:12:14 +0900

    Buiding ScheduleCard

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,33 @@ class HomeScreen extends StatefulWidget {
               ),
               TodayBanner(
                   selectedDay: selectedDay,
-                  taskCount: 0,)
+                  taskCount: 0,
+              ),
+              Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                    child: ListView(
+                      children: [
+                        ScheduleCard(
+                            startTime: DateTime(
+                              2024,
+                              07,
+                              19,
+                              11,
+                            ),
+                            endTime: DateTime(
+                              2024,
+                              07,
+                              19,
+                              12,
+                            ),
+                            content: 'Study Flutter',
+                            color: Colors.blue,
+                        ),
+                      ],
+                    ),
+                  ),
+              ),
             ],
           )
         ),
